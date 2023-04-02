79d2d116100963ef2aaf7f1ca75b0975379f547e
Cole Hersh <95268162+ColeHersh@users.noreply.github.com>
2023-04-02 13:46:50 -0700

    Update geofence.py

--- geofence.py
+++ geofence.py
@@ -26,11 +26,11 @@ async def run():
 
     # Fetch the home location coordinates, in order to set a boundary around
     # the home location.
-    print("Fetching home location coordinates...")
-    async for terrain_info in drone.telemetry.home():
-        latitude = terrain_info.latitude_deg
-        longitude = terrain_info.longitude_deg
-        break
+   # print("Fetching home location coordinates...")
+    #async for terrain_info in drone.telemetry.home():
+       # latitude = terrain_info.latitude_deg
+       # longitude = terrain_info.longitude_deg
+       # break
 
     await asyncio.sleep(1)
 
